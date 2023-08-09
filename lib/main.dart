13d7416964f7828d5e9ba83174f9e512d0131551
ahmed <hm1933018@gmail.com>
2023-08-09 14:43:26 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -17,89 +17,132 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class FacebookApp extends StatelessWidget {
-  const FacebookApp({ Key? key }) : super(key: key);
+  const FacebookApp({Key? key}) : super(key: key);
 
-  final String title;
-   @override
-   State<MyHomePageState> createstate() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage>{
-  int _counter =0;
-
-  void _incrementCounter(){
-    setState(() {
-      _counter++;
-    });
-  }
-}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          color: const Color.fromARGB(255, 195, 202, 196),
-
-      ),
-
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 255, 253, 252),
-        title: Text(      
-          "facebook" ,
-           style:  TextStyle(color: Colors.blue[900]) ,
-
-                              ),
-         Text(
-          `$_counter`,
-          style: Theme.of(context).textTeme.headlineMedium,
-         ) ,
-         floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: `Incre`,
-         )                    
-           
-        centerTitle: true,
-        leading:  IconButton(
-          icon: Icon(Icons.menu),
-          color: Colors.blue[900],
- 
-          
-          onPressed: () {  },
+        appBar: AppBar(
+          title: Text("facebook",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 27,
+                  fontWeight: FontWeight.w700)),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.blueAccent,
+              size: 30,
+            ),
+            onPressed: () {},
           ),
-
-        actions: [
-          IconButton(
-          icon: Icon(Icons.search), 
-          color: Colors.blue[900],
-          onPressed: () {  },
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.message,
+                  color: Colors.blueAccent,
+                  size: 30,
+                ),
+                onPressed: () {}),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.blueAccent,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+          ],
+          backgroundColor: Colors.white,
+          elevation: 20,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Text(
+                  "ahmad",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22.0,
+                    height: 1.5,
+                  ),
+                ),
+                height: 222,
+                width: 299,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 200,
+                child: Center(),
+              ),
+              Container(
+                child: Text(
+                  "elsayed",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22.0,
+                    height: 1.5,
+                  ),
+                ),
+                height: 222,
+                width: 299,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                child: Text(
+                  "ahmad",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22.0,
+                    height: 1.5,
+                  ),
+                ),
+                height: 122,
+                width: 122,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                child: Icon(Icons.favorite, color: Colors.red, size: 55),
+                height: 122,
+                width: 122,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red[100],
+                ),
+              ),
+            ],
           ),
-
-          IconButton(
-          icon: Icon(Icons.message), 
-          color: Colors.blue[900],
-
-          
-          onPressed: () {  }
-          ),
-
-
-          
-
-
-
-
-        ],
-        
-
-
-
-      ),
-
-      
-  
-    
-      
-    );
+        ));
   }
 }
+
+// mainAxisAlignment → MainAxisAlignment.
+// crossAxisAlignment → CrossAxisAlignment.
